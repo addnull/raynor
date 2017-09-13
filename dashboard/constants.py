a2c2c0cce2d625e242b1c977fe7384833240e495
Jongwon(Jerry) Han <addnull@gmail.com>
2017-09-13 18:03:31 +0900

    move repo

--- dashboard/constants.py
+++
@@ -1,3 +0,0 @@
-class Message:
-    STREAM_CLOSED = 0
-    STREAM_OPENED = 1
--- dashboard/consumers.py
+++
@@ -1,9 +0,0 @@
-from channels import Group
-
-
-def connect_log_stream(message):
-    Group('stream').add(message.reply_channel)
-
-
-def disconnect_log_stream(message):
-    Group('stream').discard(message.reply_channel)
